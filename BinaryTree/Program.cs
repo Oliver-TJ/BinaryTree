a6878d615a53570b2548c4fa96b0a13ffe1f15b2
Oliver Taite Jupp <07otaite-jupp@brightoncollege.net>
2021-09-20 13:04:50 +0100

    Initial commit

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class tree
+    {
+        private int[,] treeValues = new int[6,3];
+
+        public tree()
+        {
+            
+        }
+        
+    }
+    
+}
